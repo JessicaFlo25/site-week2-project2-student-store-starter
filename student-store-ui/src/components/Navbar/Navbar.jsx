23f9a7f5ba0fa701dca30401999662266643db35
JessicaFlo25 <jessica.flores309@myhunter.cuny.edu>
2023-06-16 23:14:27 -0700

    Jessica Flores Olmos Project2

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import * as React from "react"
 import "./Navbar.css"
+// import Logo from "..Logo/Logo"
+// import {Link} from 'react-router-dom'
 
 export default function Navbar() {
+
+  
   return (
     <nav className="navbar">
       <p>Navbar</p>
+      {/* <Logo/> */}
     </nav>
   )
 }
